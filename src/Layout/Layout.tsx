830d1c9ed54aa1167be1b152bf57a17bab9c0fdd
Jorge Arturo Arellano del Aguila <jorch1910@gmail.com>
2021-01-16 14:36:06 -0700

    Layout skeleton

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,6 +1,61 @@
 import * as React from 'react'
 import '../styles/less/index.less'
+import { Layout, Menu } from 'antd'
+import { HomeOutlined, UserOutlined, BookOutlined, TeamOutlined, FileDoneOutlined, BarsOutlined, CarryOutOutlined } from '@ant-design/icons'
+import { MenuOption } from '../utils/globalTypes'
+import styles from './Layout.module.scss'
 
-const Layout: React.FC = ({ children }) => <div>{children}</div>
+const { Sider, Header, Content, Footer } = Layout
 
-export default Layout
+const menuItems: Array<MenuOption> = [
+  {
+    name: 'Home',
+    icon: HomeOutlined
+  },
+  {
+    name: 'About me',
+    icon: UserOutlined
+  },
+  {
+    name: 'Services',
+    icon: UserOutlined
+  },
+  {
+    name: 'Educational',
+    icon: BookOutlined
+  },
+  {
+    name: 'Work',
+    icon: TeamOutlined
+  },
+  {
+    name: 'Skills',
+    icon: FileDoneOutlined
+  },
+  {
+    name: 'Recomendation',
+    icon: CarryOutOutlined
+  },
+  {
+    name: 'Contact me',
+    icon: BarsOutlined
+  }
+]
+
+const LayoutPage: React.FC = ({ children }) => (
+  <Layout className={styles.container}>
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div>LOGO </div>
+      <Menu>{/* {menuItems.map(item => (
+          <Menu.Item key={item.name} icon={item.icon} />
+        ))} */}</Menu>
+    </Sider>
+    <Layout>
+      <Header style={{ padding: 0 }}>Header component</Header>
+      <Content style={{ margin: '24px 16px 0' }}>{children}</Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
+)
+
+export default LayoutPage
