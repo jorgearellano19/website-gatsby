2ba09baa205ee33559899e3a056cc9215206d24a
Jorge Arturo Arellano del Aguila <jorch1910@gmail.com>
2021-01-16 11:45:11 -0700

    Variables modified

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -22,10 +22,8 @@ export const colors = {
 }
 
 export const fonts = {
-  sansSerif:
-    '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif',
-  serif: 'Georgia, "Times New Roman", Times, serif',
-  monospace: 'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace, monospace'
+  sansSerif: '-apple-system, EBGaramond sans-serif',
+  serif: ' EBGaramond serif'
 }
 
 export const breakpoints = {
